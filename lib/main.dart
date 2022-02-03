3b3da84be7c2b301600cbeb0fefdd52f8f4974d7
kirubel adamu <kirubeladamu@gmail.com>
2022-02-03 16:33:39 +0300

    changed font to consolas

--- lib/main.dart
+++ lib/main.dart
@@ -15,53 +15,78 @@ class BrainFucked extends StatefulWidget {
 class _BrainFuckedState extends State<BrainFucked> {
   Compiler compiler = Compiler();
   TextEditingController controller = TextEditingController();
-  String output = '';
+  Widget output = const SelectableText('');
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BrainF*cked',
+      theme:
+          ThemeData(primarySwatch: Colors.lightGreen, fontFamily: "Consolas"),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: controller,
-                  keyboardType: TextInputType.multiline,
-                  expands: true,
-                  maxLines: null,
-                  onEditingComplete: () {},
-                  enableInteractiveSelection: true,
-                  decoration: const InputDecoration(
-                    isDense: true,
+        appBar: AppBar(
+          title: const Text('BrainF*cked Compiler'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    keyboardType: TextInputType.multiline,
+                    expands: true,
+                    maxLines: null,
+                    onEditingComplete: () {},
+                    enableInteractiveSelection: true,
+                    decoration: const InputDecoration(
+                        isDense: true, hintText: 'Write your code here...'),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 50,
-                child: Row(
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          String? result =
-                              compiler.compile(controller.value.text);
-                          setState(() {
-                            output = result ?? 'Error';
-                          });
-                        },
-                        child: const Text('Run')),
-                  ],
+                SizedBox(
+                  height: 50,
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          style:
+                              ElevatedButton.styleFrom(primary: Colors.yellow),
+                          onPressed: () {},
+                          child: const Text('Save Code')),
+                      ElevatedButton(
+                          style:
+                              ElevatedButton.styleFrom(primary: Colors.green),
+                          onPressed: () {
+                            String? result =
+                                compiler.compile(controller.value.text);
+                            setState(() {
+                              output = result != null
+                                  ? SelectableText(result)
+                                  : const Text(
+                                      'Error',
+                                      style: TextStyle(color: Colors.red),
+                                    );
+                            });
+                          },
+                          child: const Text('Run')),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 200,
-                child: ListView(
-                  children: [
-                    SelectableText(output),
-                  ],
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Output'),
                 ),
-              )
-            ],
+                SizedBox(
+                    height: 200,
+                    child: Card(
+                      child: ListView(
+                        children: [
+                          Padding(padding: EdgeInsets.all(8.0), child: output)
+                        ],
+                      ),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
