f2ed4518b656240debbd2b4a620d35c841c4cd95
kirubel adamu <kirubeladamu@gmail.com>
2022-02-07 16:25:58 +0300

    bugfix: wrap content removed from editor

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import "package:scrollable_positioned_list/scrollable_positioned_list.dart";
 // TODO: input proccessor
 // TODO: infinite loop detector
 // TODO: keyboard hits should work
-
 void main() {
   runApp(const BrainFucked());
 }
@@ -122,39 +121,47 @@ class _BrainFuckedAppState extends State<BrainFuckedApp> {
                                       ),
                                       const VerticalDivider(),
                                       Expanded(
-                                        child: TextField(
-                                          scrollPhysics:
-                                              const NeverScrollableScrollPhysics(),
-                                          controller: controller,
-                                          keyboardType: TextInputType.multiline,
-                                          onEditingComplete: () {},
-                                          maxLines: null,
-                                          focusNode: focusNode,
-                                          minLines: null,
-                                          autofocus: false,
-                                          expands: true,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              lines = value.characters
-                                                      .where((p0) => p0 == '\n')
-                                                      .length +
-                                                  1;
-                                              characters =
-                                                  value.characters.length;
-                                            });
-                                          },
-                                          enableInteractiveSelection: true,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subtitle1!
-                                              .copyWith(
-                                                  height: 1.2,
-                                                  letterSpacing: 1.5),
-                                          decoration: const InputDecoration(
-                                              border: InputBorder.none,
-                                              isDense: true,
-                                              hintText:
-                                                  'Write your code here...'),
+                                        child: SingleChildScrollView(
+                                          scrollDirection: Axis.horizontal,
+                                          child: SizedBox(
+                                            width: 10000,
+                                            child: TextField(
+                                              scrollPhysics:
+                                                  const NeverScrollableScrollPhysics(),
+                                              controller: controller,
+                                              keyboardType:
+                                                  TextInputType.multiline,
+                                              onEditingComplete: () {},
+                                              maxLines: null,
+                                              focusNode: focusNode,
+                                              minLines: null,
+                                              autofocus: false,
+                                              expands: true,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  lines = value.characters
+                                                          .where((p0) =>
+                                                              p0 == '\n')
+                                                          .length +
+                                                      1;
+                                                  characters =
+                                                      value.characters.length;
+                                                });
+                                              },
+                                              enableInteractiveSelection: true,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .subtitle1!
+                                                  .copyWith(
+                                                      height: 1.2,
+                                                      letterSpacing: 1.5),
+                                              decoration: const InputDecoration(
+                                                  border: InputBorder.none,
+                                                  isDense: true,
+                                                  hintText:
+                                                      'Write your code here...'),
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ],
